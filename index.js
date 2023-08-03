d42d18fbb965c558c1c58b86cb9c3d0b0c8188ee
Troy Mason <troymason222@gmail.com>
2023-08-03 14:17:57 -0400

    Added JS functionality for email and button

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function contact (event){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += "modal__overlay--visible";
+    emailjs
+    .sendForm('service_yml4u4n', 'template_s3chdi9',
+    event.target,
+    'user_5120678fQdOqZJG5'
+    ) .then(()=>{
+        loading.classList.remove("modal__overlay--visible");
+        success.classList +=" modal__overlay--visible";
+    }) .catch(()=>{
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable"
+        )
+    })
+}
+let isModalOpen=false;
+function toggleModal(){
+    if(isModalOpen){
+        isModalOpen=false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen=true;
+    document.body.classList += "modal--open";
+}
